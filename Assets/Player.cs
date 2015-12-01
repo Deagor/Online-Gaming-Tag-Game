8f0a725a386712efd67653e1a93919bc8962508c
Redbeard2794 <jayo2794@gmail.com>
2015-12-01 12:49:18 +0000

    Player serializes itself and ready to send

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
@@ -11,13 +15,9 @@ public class Player : MonoBehaviour {
     public bool isPlayer1;
 
     //are we the chaser?
-    bool isChaser;
+    public bool isChaser;
 
-    public Texture p1Texture;
-
-    public Material p1Material;
-
-    GameObject player;
+    List<byte> _updateMessage = new List<byte>();
 
     // Use this for initialization
     void Start () {
@@ -27,58 +27,70 @@ public class Player : MonoBehaviour {
         startPos = transform.position;
         prevPos = transform.position;
 
-        isPlayer1 = true;
-
-        player = GameObject.Find("player1");
-
-        SpriteRenderer my_renderer = player.GetComponent<SpriteRenderer>();
-
-        if (isPlayer1 == true)
-        {
-            //player.GetComponent<SpriteRenderer>().material.SetTexture("_MainTex", p1Texture);
-            //Debug.Log(player.GetComponent<Renderer>().material.);
-            player.GetComponent<Renderer>().material.mainTexture = p1Texture;
-            //my_renderer.material = p1Material;
-        }
-        else
-        {
-            
-        }
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //access the position as you can not modify it directly?
-        Vector3 pos = transform.position;
 
-        if (Input.GetKey("up"))
+        if (isPlayer1 == true)
         {
-            pos.y += speed * Time.deltaTime;
-        }
+            //access the position as you can not modify it directly?
+            Vector3 pos = transform.position;
 
-        if (Input.GetKey("down"))
-        {
-            pos.y -= speed * Time.deltaTime;
-        }
+            if (Input.GetKey("up"))
+            {
+                pos.y += speed * Time.deltaTime;
+            }
 
-        if (Input.GetKey("right"))
-        {
-            pos.x += speed * Time.deltaTime;
-        }
+            if (Input.GetKey("down"))
+            {
+                pos.y -= speed * Time.deltaTime;
+            }
 
-        if (Input.GetKey("left"))
-        {
-            pos.x -= speed * Time.deltaTime;
-        }
+            if (Input.GetKey("right"))
+            {
+                pos.x += speed * Time.deltaTime;
+            }
 
-        //set the position to be the modified position
-        transform.position = pos;
+            if (Input.GetKey("left"))
+            {
+                pos.x -= speed * Time.deltaTime;
+            }
 
-        //set the previous position(for not sending loads of update messages later)
-        if (prevPos != transform.position)
-        {
-            prevPos = transform.position;
+            //set the position to be the modified position
+            transform.position = pos;
+
+            //set the previous position(for not sending loads of update messages later)
+            if (prevPos != transform.position)
+            {
+                prevPos = transform.position;
+                SerializeAndSend();
+            }
         }
+    }
+
+    //serialize the players position and get ready to send it
+    void SerializeAndSend()
+    {
+        _updateMessage.Clear();
+        _updateMessage.Add((byte)'U');
+
+        //serialize the player
+        _updateMessage.AddRange(System.BitConverter.GetBytes(transform.position.x));
+        _updateMessage.AddRange(System.BitConverter.GetBytes(transform.position.y));
+
+        byte[] messageToSend = _updateMessage.ToArray();
+
+        Debug.Log("My serialized message: " + messageToSend);
+
+        byte[] buffer = new byte[1024];
+        Stream stream = new MemoryStream(buffer);
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, messageToSend);
+
+        int bufferSize = 1024;
+
+        //send the serialised information so that net can send it on then
+        //net.GetComponent<NetClientHost>().SendUpdateMessage(buffer, bufferSize);
     }
 }
